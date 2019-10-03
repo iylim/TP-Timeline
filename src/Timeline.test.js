db07e765c7b6e391b9eda8f8f1246bc607abdc1e
Ivy <yunjilim@gmail.com>
2019-10-03 14:16:34 -0700

    first test written

--- src/Timeline.test.js
+++ src/Timeline.test.js
@@ -3,9 +3,8 @@ import { shallow } from 'enzyme';
 import Timeline from './Timeline';
 
 describe('Timeline', () => {
-  it('should render', () => {
+  it('should render correctly in Debug', () => {
     const component = shallow(<Timeline debug />);
-  
     expect(component).toMatchSnapshot();
   })
 })
